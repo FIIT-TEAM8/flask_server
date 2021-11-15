eac8f165f2f28ddbe56a9ff6d69003c2b1865b43
Dominik Horváth <dom.horvath17@gmail.com>
2021-11-15 19:10:56 +0100

    dummy api created

---
+++ api/v1/api_settings.py
@@ -0,0 +1,8 @@
+API_VERSION = "v1"
+
+# API parameters
+# /search endpoint
+API_SEARCH_QUERY = "q"
+API_SEARCH_FROM = "from"
+API_SEARCH_TO = "to"
+API_SEARCH_LOCALE = "locale"
--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask
+from api.v1.search_routes import search_api
 
 app = Flask(__name__)
 
+app.register_blueprint(search_api)
+
+
 @app.route("/")
-def hello():
-    return "<h1>Hello world from flask</h1>"
+def root():
+    return "<h1>/v1/search - entrypoint for searching</h1>"
+
+
+if __name__ == '__main__':
+    app.run()
