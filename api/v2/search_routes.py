2bbc2a77d0147148a1366195e2ae6d6c391063d0
Jakub Müller <jakub.muller.so@gmail.com>
2021-12-04 13:59:10 +0100

    successful ids retrieval

--- api/v2/search_routes.py
+++ api/v2/search_routes.py
@@ -1,13 +1,27 @@
 from flask import Blueprint
 import api.v2.api_settings as api_settings
 from flask import request
-from random import randint
+from elasticsearch import Elasticsearch
 
 # /v2/search/
 search_api = Blueprint("search_routes", __name__, url_prefix="/" + api_settings.API_VERSION + "/search")
 
+# es = Elasticsearch('https://team08-21.studenti.fiit.stuba.sk/es01/')
 
-@search_api.route("/")
+es = Elasticsearch(hosts=[
+    {"host":'es01'}
+])
+
+def get_ids(result):
+    ids = []
+
+    for res in result["hits"]["hits"]:
+        ids.append(res["_id"])
+
+    return ids
+
+
+@search_api.route("/", methods=['GET'])
 def search():
     query = request.args.get(api_settings.API_SEARCH_QUERY, default=None, type=str)
     search_from = request.args.get(api_settings.API_SEARCH_FROM, default="", type=str)
@@ -17,29 +31,22 @@ def search():
     if query is None:
         return "Invalid input, please provide 'q' parameter", 400
 
-    f = open("sample_lipsum.txt")
-
-    articles = []
-    art_body = f.read()
-    art_count = randint(5, 10)
-
-    for i in range(0, art_count):
-        article = {
-            "link": "https://thispersondoesnotexist.com/",
-            "published": "2020-01-01",
-            "locale": "en-gb",
-            "title": "Sample article number " + str(i),
-            "body": art_body
+    body = {
+        "query": {
+            "multi_match": {
+                "query": query,
+                "fields": ["text"]
+            }
         }
-        articles.append(article)
+    }
+
+    resp = es.search(index="articles_index", doc_type="_doc", body=body, size=10)
+
+    articles_ids = get_ids(resp)
 
     response = {
         "query": query,
-        "search_from": search_from,
-        "search_to": search_to,
-        "locale": locale,
-        "result_count": art_count,
-        "results": articles
+        "articles_ids": articles_ids
     }
 
     return response
