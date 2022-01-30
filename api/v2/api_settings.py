99efe718be8152234f4b1663fc6f759418975f1c
Dominik Horváth <dom.horvath17@gmail.com>
2022-01-30 15:46:17 +0100

    refactor

--- api/v2/api_settings.py
+++ api/v2/api_settings.py
@@ -11,5 +11,5 @@ API_SEARCH_LOCALE = "locale"
 
 
 # environmental variables
-MONGO_INITDB_ROOT_USERNAME = os.environ['MONGO_INITDB_ROOT_USERNAME'] or 'root',
-MONGO_INITDB_ROOT_PASSWORD = os.environ['MONGO_INITDB_ROOT_PASSWORD'] or 'password'
+MONGO_INITDB_ROOT_USERNAME = str(os.environ['MONGO_INITDB_ROOT_USERNAME'] or 'root')
+MONGO_INITDB_ROOT_PASSWORD = str(os.environ['MONGO_INITDB_ROOT_PASSWORD'] or 'password')
