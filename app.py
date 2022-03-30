25cee2cacea5de27fdb067ad4e4d5a40e61fa47f
Dominik Horváth <dom.horvath17@gmail.com>
2022-03-30 19:35:33 +0200

    dev refactor

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
+import os
+from dotenv import load_dotenv
+if os.path.exists("./.env"):
+    load_dotenv()
 from flask import Flask
 from api.v1.search_routes import search_api as search_api_v1
 from api.v2.search_routes import search_api as search_api_v2
 from api.v3.search_routes import search_api as search_api_v3
 from api.v2.json_encoder import MyEncoder
+from dotenv import load_dotenv
+import os
+
 
 app = Flask(__name__)
 
