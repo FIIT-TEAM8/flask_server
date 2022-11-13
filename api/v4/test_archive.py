08f375fdbff2676cea9880ed88148bf49619273b
Jakub Müller <jakub.muller.so@gmail.com>
2022-11-13 20:58:09 +0100

    added archive tests

---
+++ api/v4/test_archive.py
@@ -0,0 +1,67 @@
+import unittest
+from unittest import mock
+from flask import Flask
+import json
+
+app = Flask(__name__)
+
+databaseResponse = {
+      "_id": "6239b1eddf4b7decb33fbaf2",
+      "html": "scraped html",
+      "keywords":["Assassination"],
+      "language":"en",
+      "link": "https://test.com/",
+      "published": "2020-01-01",
+      "region":"gb",
+      "title": "Sample article number 0"
+    }
+
+serverResponse =  {
+  "article": 
+    {
+      "_id": "6239b1eddf4b7decb33fbaf2",
+      "html": "scraped html",
+      "keywords":["Assassination"],
+      "language":"en",
+      "link": "https://test.com/",
+      "published": "2020-01-01",
+      "region":"gb",
+      "title": "Sample article number 0"
+    }
+}
+
+from api.v4.search_routes import api_v4
+app.register_blueprint(api_v4, name="api_v4")
+
+class TestArchiveRoutes(unittest.TestCase):
+
+    def setUp(self):
+        self.app = app.test_client()
+
+    def test_no_link(self):
+        response = self.app.get('api/v4/archive')
+        assert response.data.decode("utf-8")  == "Invalid input, please provide 'link' parameter"
+
+    @mock.patch("api.v4.search_routes.Database.find_one", return_value=databaseResponse)
+    def test_archive(self, article):
+        response = self.app.get('api/v4/archive?link=https://www.test.com/')
+        resp_json = response.data.decode('utf8').replace("'", '"')
+        resp_data = json.loads(resp_json)
+
+        print(resp_data)
+
+        assert resp_data == serverResponse
+
+
+    # @mock.patch("api.v4.search_routes.Database.find_one", return_value=None)
+    # def test_archive_article_doesnt_exist(self, conn, article):
+    #     response = self.app.get('api/v4/archive?link=https://www.test.com/')
+    #     resp_json = response.data.decode('utf8').replace("'", '"')
+    #     resp_data = json.loads(resp_json)
+
+    #     print(resp_data)
+
+    #     assert resp_data == "Article does not exist"
+
+if __name__ == "__main__":
+    unittest.main()
--- api/v4/test_search.py
+++ api/v4/test_search.py
@@ -1,35 +1,11 @@
 import unittest
-import requests
 from unittest import mock
 from flask import Flask
 import json
 
 app = Flask(__name__)
 
-foundArticle = {
-    "page_num": 1,
-    "per_page": 10,
-    "query": "test",
-    "results": [
-        {
-            "_id": "864257bh4ec0b85j6er83751",
-            "keywords": ["test"],
-            "language": "sk",
-            "link": ["www.test.com"],
-            "preview": "preview",
-            "published": ["Sun, 06 Nov 2022 16:34:20 GMT"],
-            "region": "sk",
-            "title": ["Test"],
-        }
-    ],
-    "search_from": "",
-    "search_to": "",
-    "total_pages": 1,
-    "total_results": 1,
-}
-
-
-article_test = {
+elasticResponse = {
     "hits": {
         "total": {
             "value": 1
@@ -37,7 +13,7 @@ article_test = {
         "hits": [
             {
                 "_source": {
-                    "html": "sdfsdf"
+                    "html": "test html"
                 },
                 "_id": "864257bh4ec0b85j6er83751"
             }
@@ -45,31 +21,55 @@ article_test = {
     }
 }
 
+serverResponse = {
+    "query": "test",
+    "search_from": "",
+    "search_to": "",
+    "page_num": 1,
+    "per_page": 1,
+    "total_pages": 1,
+    "total_results": 1,
+    "results": [
+        {
+            '_id': '864257bh4ec0b85j6er83751',
+            'preview': 'Article preview is currently not supported.'
+        }
+    ]
+}
+
 from api.v4.search_routes import api_v4
 app.register_blueprint(api_v4, name="api_v4")
 
-from api.v4.search_routes import search
-
-
-class TestClassIWantToTest(unittest.TestCase):
+class TestSearchRoutes(unittest.TestCase):
 
     def setUp(self):
         self.app = app.test_client()
 
-    def test_health(self):
-        rv = self.app.get('api/v4/search')
-        assert rv.data.decode("utf-8")  == "Invalid input, please provide 'q' parameter"
+    def test_no_query(self):
+        response = self.app.get('api/v4/search')
+        assert response.data.decode("utf-8")  == "Invalid input, please provide 'q' parameter"
     
 
     @mock.patch("api.v4.search_routes.Elastic.check_connection", return_value=200)
-    @mock.patch("api.v4.search_routes.Elastic.search", return_value=article_test)
-    def test_conn(self, lol, art):
-        rv = self.app.get('api/v4/search?q=murder')
-        my_json = rv.data.decode('utf8').replace("'", '"')
-        data = json.loads(my_json)
-        print(data["results"][0]["_id"])
+    @mock.patch("api.v4.search_routes.Elastic.search", return_value=elasticResponse)
+    def test_search(self, conn, article):
+        response = self.app.get('api/v4/search?q=test')
+        resp_json = response.data.decode('utf8').replace("'", '"')
+        resp_data = json.loads(resp_json)
 
-        assert data["results"][0]["_id"] == article_test["hits"]["hits"][0]["_id"]
+        print(resp_data)
+
+        assert resp_data == serverResponse
+
+
+    # @mock.patch("api.v4.search_routes.Elastic.check_connection", return_value=200)
+    # @mock.patch("api.v4.search_routes.Elastic.search", return_value=elasticResponse)
+    # def test_advanced_search(self, conn, article):
+    #     response = self.app.get('api/v4/search?q=murder')
+    #     resp_json = response.data.decode('utf8').replace("'", '"')
+    #     resp_data = json.loads(resp_json)
+
+    #     assert resp_data["results"][0]["_id"] == elasticResponse["hits"]["hits"][0]["_id"]
 
 if __name__ == "__main__":
     unittest.main()
